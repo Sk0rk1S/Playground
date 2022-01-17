f81eef53ded837691a86b1f38d2f288c5f418596
Sk0rk1S <91293863+Sk0rk1S@users.noreply.github.com>
2022-01-16 19:09:33 -0800

    Create Randomshit.sol

---
+++ Randomshit.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./aave/FlashLoanReceiverBase.sol";
+import "./aave/ILendingPoolAddressesProvider.sol";
+import "./aave/ILendingPool.sol";
+
+contract Ethgen is FlashLoanReceiverBase {
+   uint ETHDNA = 256;
+   uint ETHMODULUS = 10 ** ETHDNA;
+   
+   struct Ethman {
+      string name;
+      uint ETHDNA;
+   }
+   
+   ETH[] public ETHER;
+   
+   function _createEth(string _memory, uint _ETHDNA) private {
+   ETH.push(public(ETHER));
+   }
+   
+   function _executeloan(string _memory, uint _name) private : {
+   function _getloan(string _memory, uint _ETHDNA) private view returns (uint);
+   if
+     FlashLoanReceiverBase =! ILendingPool
+   uint amount = 1 ether;
+   require(uint)
+   
+   } 
+}
